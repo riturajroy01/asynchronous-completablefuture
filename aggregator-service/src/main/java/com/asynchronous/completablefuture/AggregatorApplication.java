e8b5717c0ad0d1044c2dcc98a9aa7e8306ce26f9
riturajroy01 <web12345678>
2025-10-22 03:10:42 +0530

    second service commit

--- aggregator-service/src/main/java/com/asynchronous/completablefuture/AggregatorApplication.java
+++ aggregator-service/src/main/java/com/asynchronous/completablefuture/AggregatorApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class CompletablefutureApplication {
+public class AggregatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompletablefutureApplication.class, args);
+		SpringApplication.run(AggregatorApplication.class, args);
 	}
 
 }
